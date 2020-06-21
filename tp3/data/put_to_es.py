77c8114c45341b29a5c461176ddd32de55742fd4
Mehdi OUAGHLANI <mouaghlani@octo.com>
2020-06-21 13:39:51 +0200

    MAJ script indexation TP3

--- tp3/data/put_to_es.py
+++ tp3/data/put_to_es.py
@@ -1,19 +1,21 @@
-from pyes import ES
+from elasticsearch import Elasticsearch
 import json
 import sys
 
-conn = ES()
-c = 1
-
+es = Elasticsearch()
+counter = 1
 
 def log(c):
-	sys.stdout.write(str(c)+' documents indexed \r')
-	sys.stdout.flush()
+        sys.stdout.write(str(counter)+' documents indexed \r')
+        sys.stdout.flush()
 
 with open('data.txt') as f:
-	for line in f.readlines():
-		line_id = c
-		log(c)
-		c += 1
-		line = json.loads(line)
-		conn.index(line,  'comics', 'comic', line_id)
+        for line in f.readlines():
+                line_id = counter
+                log(counter)
+                counter += 1
+                line = json.loads(line)
+                print(line)
+                es.index(index="comics", id=line_id, body=line)
+
+print("Data loaded in Elasticsearch within tweet index")
